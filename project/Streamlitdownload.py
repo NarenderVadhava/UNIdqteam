6b60b2bc71f37ba3b06c6c34813109ca351e20e1
nv77707 <nv_nkv@yahoo.co.in>
2026-03-19 15:34:59 +0530

    Streamlitdownload.py

--- project/Streamlitdownload.py
+++ project/Streamlitdownload.py
@@ -3,12 +3,10 @@ import streamlit as st
 text_content = "This is a text file."
 
 bytes_data = text_content.encode("utf-8")
-
-st.write("Running start")
+st.header(text_content)
 #st.download_button(
  #   label="Download (bytes)",
   #  data=bytes_data,
    # file_name="data.txt",
     #mime="text/plain"
 #)
-st.write("Running done.")
