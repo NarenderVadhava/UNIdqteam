4826e86ad00455a86c782020664ab4435c62ddd0
nv77707 <nv_nkv@yahoo.co.in>
2026-03-19 15:36:03 +0530

    Streamlitdownload.py

--- project/Streamlitdownload.py
+++ project/Streamlitdownload.py
@@ -4,9 +4,9 @@ text_content = "This is a text file."
 
 bytes_data = text_content.encode("utf-8")
 st.header(text_content)
-#st.download_button(
- #   label="Download (bytes)",
-  #  data=bytes_data,
-   # file_name="data.txt",
-    #mime="text/plain"
-#)
+st.download_button(
+    label="Download (bytes)",
+    data=bytes_data,
+    file_name="data.txt",
+    mime="text/plain"
+)
