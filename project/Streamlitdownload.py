e9817c337ee1e6978288628afcc903df63316456
nv77707 <nv_nkv@yahoo.co.in>
2026-03-19 15:31:34 +0530

    Streamlitdownload.py

--- project/Streamlitdownload.py
+++ project/Streamlitdownload.py
@@ -4,11 +4,11 @@ text_content = "This is a text file."
 
 bytes_data = text_content.encode("utf-8")
 
-st.print("Running start")
+st.write("Running start")
 st.download_button(
     label="Download (bytes)",
     data=bytes_data,
     file_name="data.txt",
     mime="text/plain"
 )
-st.print("Running done.")
+st.write("Running done.")
